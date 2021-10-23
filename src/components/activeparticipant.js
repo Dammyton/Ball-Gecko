239cb75f3ca8bc3108601a6806ec2f07a29ec9f0
Damilola Owolabi <owolabidamilola98@gmail.com>
2021-10-23 18:20:21 +0100

    added the files

--- src/components/activeparticipant.js
+++ src/components/activeparticipant.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Slider, ActiveParticipantCard } from "./Slider";
-import union from "../assets/Union.png";
+import union from "../assets/Group.png";
 import participant1 from "../assets/participant1.png";
 
 const participants = [
@@ -11,21 +11,26 @@ const participants = [
 
 const ActiveParticipants = () => {
   return (
-    <div className=" px-3 pt-3">
-      <div className="row mb-2">
-        <div className="col-2">
-          <img src={union} height="52px" />
+    <div className="container">
+      <div className=" pt-3 px-3">
+        <div className="row mb-2">
+          <div className="col-2">
+            <img src={union} height="52px" alt="union"/>
+          </div>
+          <div
+            className="col-3"
+            style={{ color: "#7F7F7F", letterSpacing: "1px" }}
+          >
+            Invite Someone
+          </div>
         </div>
-        <div className="col-3" style={{ color: "#7F7F7F",letterSpacing: "1px" }}>
-          Invite Someone
-        </div>
-      </div>
 
-      <Slider>
-        {participants.map((participant, index) => (
-          <ActiveParticipantCard key={index} image={participant.image} />
-        ))}
-      </Slider>
+        <Slider>
+          {participants.map((participant, index) => (
+            <ActiveParticipantCard key={index} image={participant.image} />
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
--- src/components/chat.js
+++ src/components/chat.js
@@ -5,79 +5,77 @@ import image from "../assets/Image.png";
 
 const Chat = () => {
   return (
-    <div className=" pb-3 px-3">
-      <p
-        className="text-center mb-1"
-        style={{ color: "#ED1C24", letterSpacing: "1px", fontSize: "12px" }}
-      >
-        TODAY 6:34 PM
-      </p>
+    <div className="container">
+      <div className=" pb-3 px-3">
+        <p
+          className="text-center mb-1"
+          style={{ color: "#ED1C24", letterSpacing: "1px", fontSize: "12px" }}
+        >
+          TODAY 6:34 PM
+        </p>
 
-      <div className="row mb-1">
-        <div className="col-3"></div>
-        <div className="col-9 position-relative">
-          <div className="bg-dark text-white p-2 ls-1">
-            Anybody here loving Lukaku’s form?
-          </div>
-          <img
-            src={avatar1}
-            height="24px"
-            alt="avatar"
-            className="position-absolute"
-            style={{ right: " 10px", bottom: " 0px" }}
-          />
-        </div>
-      </div>
-
-      <div className="row mb-1">
-        <div className="col-3"></div>
-        <div className="col-9 position-relative">
-          <div className="bg-dark text-white p-2 ls-1">
-            He is in terrible form, @halods hope say you bet on top this guy?
-          </div>
-          <img
-            src={avatar1}
-            height="24px"
-            alt="avatar"
-            className="position-absolute"
-            style={{ right: " 10px", bottom: " 0px" }}
-          />
-        </div>
-      </div>
-
-      <div className="row mb-1">
-        <div className="col-4"></div>
-        <div className="col-8 position-relative align-right ">
-          <div className="mb-1">
-            <img src={image} height="70px" alt="footballer" />
-          </div>
-          <div className="bg-dark text-white align-right p-2 ls-1">
-            Peep my GOAT’s jubilation style! Dope stuff!!!{" "}
+        <div className="row mb-1">
+          <div className="col-3"></div>
+          <div className="col-9 position-relative">
+            <div className="bg-dark text-white p-2 ls-1">
+              Anybody here loving Lukaku’s form?
+            </div>
+            <img
+              src={avatar1}
+              height="24px"
+              alt="avatar"
+              className="position-absolute"
+              style={{ right: " 10px", bottom: " 0px" }}
+            />
           </div>
         </div>
-      </div>
-      <p className="newuser-joined mb-1">Folake joined.</p>
-      <div className="row mb-1">
-        <div className="col-9 position-relative">
-          <div className="bg-dark text-white p-2 ls-1">
-            Hello everybody! I’m Folake.
-          </div>
-          <img
-            src={avatar1}
-            height="24px"
-            alt="avatar"
-            className="position-absolute"
-            style={{ right: " 10px", bottom: " 0px" }}
-          />
-        </div>
-        <div className="col-3"></div>
-      </div>
 
-      <InputGroup
-        className="mt-3"
-        style={{ background: "#252836"}}
-      >
-        <InputGroup.Text className="border-0"  style={{ background: "#252836" }}>
+        <div className="row mb-1">
+          <div className="col-3"></div>
+          <div className="col-9 position-relative">
+            <div className="bg-dark text-white p-2 ls-1">
+              He is in terrible form, @halods hope say you bet on top this guy?
+            </div>
+            <img
+              src={avatar1}
+              height="24px"
+              alt="avatar"
+              className="position-absolute"
+              style={{ right: " 10px", bottom: " 0px" }}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-1">
+          <div className="col-4"></div>
+          <div className="col-8 position-relative align-right ">
+            <div className="mb-1">
+              <img src={image} height="70px" alt="footballer" />
+            </div>
+            <div className="bg-dark text-white align-right p-2 ls-1">
+              Peep my GOAT’s jubilation style! Dope stuff!!!{" "}
+            </div>
+          </div>
+        </div>
+        <p className="newuser-joined mb-1">Folake joined.</p>
+        <div className="row mb-1">
+          <div className="col-9 position-relative">
+            <div className="bg-dark text-white p-2 ls-1">
+              Hello everybody! I’m Folake.
+            </div>
+            <img
+              src={avatar1}
+              height="24px"
+              alt="avatar"
+              className="position-absolute"
+              style={{ right: " 10px", bottom: " 0px" }}
+            />
+          </div>
+          <div className="col-3"></div>
+        </div>
+      </div>
+      <InputGroup className="mt-3" style={{ background: "#252836" }}>
+        <InputGroup.Text className="border-0" style={{ background: "#252836" }}>
           <i className="fa fa-smile-o" style={{ color: "#596787" }}></i>
         </InputGroup.Text>
         <FormControl
